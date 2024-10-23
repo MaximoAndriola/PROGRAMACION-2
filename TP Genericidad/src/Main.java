bb4503b7184515c74a12628ad526c330bc8d4343
MaximoAndriola <maximoandriola2017@gmail.com>
2024-10-23 14:04:19 -0300

    Agregados: TP JSON y simulacro 2do parcial

--- TP Genericidad/src/Main.java
+++ TP Genericidad/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.function.ToDoubleBiFunction;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,11 +15,29 @@ public class Main {
         System.out.println(numeros.buscar(5));
         */
 
+        /*
         Pila<Number> numeros = new Pila<>();
         numeros.agregar(5);
         numeros.agregar(1);
         System.out.println(numeros);
         System.out.println(numeros.cantidad());
+        */
+
+        /*
+        OperacionMatematica operacion = new OperacionMatematica();
+
+        System.out.println(operacion.suma(1, 6.4));
+        */
+        //ToDo arreglar esto que no se como se hace
+
+        /*
+        Conjunto conjunto = new Conjunto();
+        conjunto.agregar(19);
+        conjunto.agregar(20);
+        conjunto.agregar("Hola mundo");
+        conjunto.eliminar(20);
+        System.out.println(conjunto);
+        */
     }
 
 }
